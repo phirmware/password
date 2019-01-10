7ea41754e7fb8f524b834784b5c520f904a67849
phirmware <chibuzor.ojukwu@gmail.com>
2019-01-10 06:54:28 +0100

    cool

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
+var staticCacheName = "password-static-v3";
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("password-static-v1").then(cache => {
+    caches.open(staticCacheName).then(cache => {
       cache.addAll([
-        "/index.htm",
-        "/script.js",
-        "/style.css",
-        "/animate.min.css",
-        "/bootstrap.min.css",
-        "/jquery.min.js",
-        "/img/favicon.ico",
-        "/img/favicon.png"
+        "index.htm",
+        "script.js",
+        "style.css",
+        "animate.min.css",
+        "bootstrap.min.css",
+        "jquery.min.js",
+        "img/favicon.ico",
+        "img/favicon.png",
+        "img/original.jpeg"
       ]);
     })
   );
@@ -24,4 +27,24 @@ self.addEventListener("fetch", event => {
       return fetch(event.request);
     })
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      cacheNames
+        .filter(cacheName => {
+          return (
+            cacheName.startsWith("password-") && cacheName != staticCacheName
+          );
+        })
+        .map(cacheName => {
+          return caches.delete(cacheName);
+        });
+    })
+  );
+});
+
+self.addEventListener("message", event => {
+  self.skipWaiting();
 });
